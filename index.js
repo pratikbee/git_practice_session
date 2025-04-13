fcacb79db6ef970c1bfc932670204238b23b5825
Pratik Jha <pratik.jha@appinventiv.com>
2025-04-13 21:05:51 +0530

    textual changes and reference file changes

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Hello learners, welcome to the template for git and github learning.
+
+
+
+// This is a template file that should be there for every branch you push.
+// Go through the instructions given in assignment. Don't make changes to this file.
+// This will ensure consitency among all the PRs raised.
