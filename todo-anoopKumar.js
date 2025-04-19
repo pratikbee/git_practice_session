a16cc495fbf957fa955c6056d8893b3be1f7d141
anoopKumarChaudhary <anupchaudhary1021@gmail.com>
2025-04-19 10:24:24 +0530

    Commit of js function by anoopKumar

---
+++ todo-anoopKumar.js
@@ -0,0 +1,44 @@
+// Debounce Function
+function debounce(func, delay) {
+  let timer;
+  return function (...args) {
+    clearTimeout(timer);
+    timer = setTimeout(() => func.apply(this, args), delay);
+  };
+}
+
+// Throttle Function
+function throttle(func, limit) {
+  let lastFunc;
+  let lastRan;
+  return function (...args) {
+    const context = this;
+    if (!lastRan) {
+      func.apply(context, args);
+      lastRan = Date.now();
+    } else {
+      clearTimeout(lastFunc);
+      lastFunc = setTimeout(function () {
+        if ((Date.now() - lastRan) >= limit) {
+          func.apply(context, args);
+          lastRan = Date.now();
+        }
+      }, limit - (Date.now() - lastRan));
+    }
+  };
+}
+
+// Deep Copy Function (Without JSON)
+function deepCopy(obj, hash = new WeakMap()) {
+  if (Object(obj) !== obj || obj instanceof Function) return obj;
+  if (hash.has(obj)) return hash.get(obj);
+  const result = Array.isArray(obj) ? [] : {};
+  hash.set(obj, result);
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      result[key] = deepCopy(obj[key], hash);
+    }
+  }
+  return result;
+}
+
